930f8f2b8388ea124fe0f4e6f4e4587e487b6616
Mohamed-Elmougy11 <mohamed.elmougy11@gmail.com>
2024-11-02 20:25:58 +0200

    feat: complete transactions section with dummy data

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,6 +4,7 @@ import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from "./";
 import { transactionContext } from "../context/TransactionsContext";
 import { useContext } from "react";
+import { shortenAddress } from "../utils/shortenAddress";
 
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -31,7 +32,7 @@ const Welcome = () => {
     sendTransaction()
   };
   return (
-    <div className="flex w-full justify-center items-center">
+    <div className="flex w-full flex-col md:flex-row justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
@@ -39,7 +40,7 @@ const Welcome = () => {
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily on
-            Krypto.
+            Krypt.
           </p>
           {!currentAccount && (
             <button
@@ -73,7 +74,7 @@ const Welcome = () => {
               <BsInfoCircle fontSize={17} color="#fff" />
             </div>
             <div>
-              <p className="text-white font-light text-sm">Address</p>
+              <p className="text-white font-light text-sm">{shortenAddress(currentAccount)}</p>
               <p className="text-white font-semibold text-lg mt-1">Ethereum</p>
             </div>
           </div>
